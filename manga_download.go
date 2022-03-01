0c9f12ff517925633b0f02f15cfeb1fb88bcd6fd
Toan Nguyen <ntoan96@gmail.com>
2022-03-01 09:17:05 +0700

    add support for ?id

--- manga_download.go
+++ manga_download.go
@@ -26,6 +26,7 @@ func (chapters ChapterList) DownloadAsZip(dataSaver bool, prefix string, ext str
 }
 
 func commonBatchDownload(chapters ChapterList, dataSaver bool, prefix string, zip bool, ext string) bool {
+	// intialize return value
 	is_ok := true
 	if len(chapters) == 0 {
 		log.Println("Chapter list is empty")
@@ -33,10 +34,13 @@ func commonBatchDownload(chapters ChapterList, dataSaver bool, prefix string, zi
 	page_cnt := uint(0)
 	c_cnt := uint(0)
 	c := make(chan error)
+
+	// mangadex limits bandwith so if there's too many chapter, apply delay mechanism
 	delay := len(chapters) > 40
 	for id, chapter := range chapters {
 		num_pages := chapter.GetPages()
 
+		// if number of pages downloading simultaneously exceeds limit, wait for all downloads to complete first
 		if page_cnt+num_pages > PAGE_LIMIT {
 			page_cnt = 0
 			for c_cnt > 0 {
@@ -48,28 +52,36 @@ func commonBatchDownload(chapters ChapterList, dataSaver bool, prefix string, zi
 			}
 		}
 
-		prefix = strings.Replace(prefix, "#id#", fmt.Sprintf("%04d", id), -1)
 		page_cnt = page_cnt + num_pages
-		go func(chapter *ChapterData) {
+
+		// spawn download goroutine
+		go func(chapter *ChapterData, id int) {
 			var err error
+			extended_prefix := strings.Replace(prefix, "?id", fmt.Sprintf("%04d", id), -1)
 			if zip {
 				if ext == "" {
 					ext = "zip"
 				}
-				err = chapter.DownloadAsZip(dataSaver, prefix+chapter.GetChapter()+"."+ext)
+				err = chapter.DownloadAsZip(dataSaver, extended_prefix+chapter.GetChapter()+"."+ext)
 			} else {
-				err = chapter.Download(dataSaver, prefix+chapter.GetChapter())
+				err = chapter.Download(dataSaver, extended_prefix+chapter.GetChapter())
 			}
 			if err == nil {
 				log.Println("Chapter " + chapter.GetChapter() + " downloaded.")
 			}
 			c <- err
-		}(chapter)
+		}(chapter, id)
+
+		// apply delay to limit bandwith
 		if delay {
 			time.Sleep(1500 * time.Millisecond)
 		}
+
+		// increament number of active go routines
 		c_cnt++
 	}
+
+	// wait for all go routine to finished
 	for c_cnt > 0 {
 		err := <-c
 		if err != nil {
